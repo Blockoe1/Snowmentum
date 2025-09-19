081fd1bf9f72af17a090995bc9150ac495423720
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-19 13:50:04 -0500

    Movement Clamping

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -2,20 +2,13 @@
 // File Name : PlayerController.cs
 // Author : Jack Fisher
 // Creation Date : September 17th, 2025
-// Last Modified : September 17th, 2025
+// Last Modified : September 19th, 2025
 //
 // Brief Description : As of script creation this script just handles player movement. 
 // We can expand this script to handle more parts of the snowball if needed though. 
 *****************************************************************************/
-using System;
-using System.Collections;
-using System.Net.Sockets;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
-//I added a bunch of these Unity system things just so hopefully I will never be missing one. Most are currently uneccesary. 
 
 
 namespace Snowmentum
@@ -24,7 +17,7 @@ namespace Snowmentum
     {
         //We can change this during gameplay in order to make it harder to move the snowball as it grows in size
         // And also adjust it as needed to make it feel responsive enough
-        [SerializeField] private float movementSensitivity;
+        [SerializeField] private float baseMovementSensitivity;
 
         [SerializeField] private PlayerInput playerInput;
         private InputAction mouseMovement;
@@ -37,8 +30,8 @@ namespace Snowmentum
         //values used for clamping the position of the snowball
         [SerializeField] private float minY;
         [SerializeField] private float maxY;
-        private Vector2 currentY;
-        private void Start()
+
+        private void Awake()
         {
             //set the player input active
             playerInput = GetComponent<PlayerInput>();
@@ -50,14 +43,20 @@ namespace Snowmentum
             snowballRigidbody = GetComponent<Rigidbody2D>();
             
         }
-        
-        void Update()
+
+        private void OnDestroy()
+        {
+            mouseMovement.started -= Handle_SnowballMouseMovement;
+        }
+
+        void FixedUpdate()
         {
             //will keep track of the mouse and move the snowball accordingly each frame
-            MoveSnowball();
+            ApplyMovementForce();
 
             //this should clamp the snowball and prevent it from moving off the screen, but currently the snowball is just teleporting between the two positions
-            transform.position = new Vector3(Mathf.Clamp(transform.position.y, minY, maxY), transform.position.x);
+            //transform.position = new Vector3(Mathf.Clamp(transform.position.y, minY, maxY), transform.position.x);
+            ClampY();
         }
 
         ///  Player Movement
@@ -68,17 +67,26 @@ namespace Snowmentum
         //hopefully this function being public is fine
         public void Handle_SnowballMouseMovement(InputAction.CallbackContext context)
         {
-           mouseDelta = context.ReadValue<Vector2>(); ;
+            mouseDelta = context.ReadValue<Vector2>();
+            //Debug.Log(mouseDelta);
         }
 
         //this function moves the snowball up and down in accordance with the movement of the mouse
-        private void MoveSnowball()
+        private void ApplyMovementForce()
         {
-            
-            snowballRigidbody.AddForce(Vector2.up * movementSensitivity * mouseDelta.y);
+            snowballRigidbody.AddForce(Vector2.up * baseMovementSensitivity * mouseDelta.y);
 
             //Old movement that used transform.translate. Can probably remove but leaving it for now just in case, I guess
             //transform.Translate(Vector3.up * mouseDelta.y * movementSensitivity);
+        }
+
+        /// <summary>
+        /// Clamps the Y position of the snowball between the min and max values.
+        /// </summary>
+        private void ClampY()
+        {
+            float yPos = Mathf.Clamp(snowballRigidbody.position.y, minY, maxY);
+            snowballRigidbody.position = new Vector2(snowballRigidbody.position.x, yPos);
         }
     }
 }
