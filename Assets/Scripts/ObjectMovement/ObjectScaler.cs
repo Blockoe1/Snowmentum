56e2a279f33c0a7a2615155a6d0511c9bebdc2b2
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-22 11:22:08 -0500

    Prefab Update

--- Assets/Scripts/ObjectMovement/ObjectScaler.cs
+++ Assets/Scripts/ObjectMovement/ObjectScaler.cs
@@ -23,7 +23,6 @@ namespace Snowmentum
     " when it spawns.  This results in obstacles spawning at varied locations based on their size.")]
         private bool scaleOnSpawn;
 
-        private Vector3 baseSize;
         private float oldSize = 1;
 
         #region Properties
@@ -46,7 +45,6 @@ namespace Snowmentum
         private void Awake()
         {
             // Store our base size.
-            baseSize = transform.localScale;
             oldSize = scaleOnSpawn ? 1 : 0;
         }
 
@@ -114,7 +112,7 @@ namespace Snowmentum
         /// <param name="scale">The scale to set for this obstacle.</param>
         private void ScaleObstacle(float scale)
         {
-            transform.localScale = baseSize * scale;
+            transform.localScale = Vector3.one * scale;
         }
     }
 }
