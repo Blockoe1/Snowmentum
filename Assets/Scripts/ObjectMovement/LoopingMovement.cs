d8458e4acb4725490708487b7d692652b7c75c6d
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-22 12:50:47 -0500

    Sprite Updates

---
+++ Assets/Scripts/ObjectMovement/LoopingMovement.cs
@@ -0,0 +1,25 @@
+/*****************************************************************************
+// File Name : LoopingMovement.cs
+// Author : Brandon Koederitz
+// Creation Date : 9/22/2025
+// Last Modified : 9/22/2025
+//
+// Brief Description : Loops the object across the screen.
+*****************************************************************************/
+using UnityEngine;
+
+namespace Snowmentum
+{
+    public class LoopingMovement : MonoBehaviour, IMovementModifier
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetPos"></param>
+        /// <returns></returns>
+        public Vector2 MoveUpdate(Vector2 targetPos)
+        {
+            
+        }
+    }
+}
