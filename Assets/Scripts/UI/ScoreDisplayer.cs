f55a571d39f009c3f1dc76841b5cec8840b414d1
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-23 16:18:49 -0500

    Score and freeze ui first pass

--- Assets/Scripts/UI/ScoreDisplayer.cs
+++ Assets/Scripts/UI/ScoreDisplayer.cs
@@ -13,6 +13,8 @@ namespace Snowmentum.Score
 {
     public class ScoreDisplayer : MonoBehaviour
     {
+        [SerializeField] private int displayedDigits = 5;
+        [SerializeField] private int postfixDigits = 1;
         #region Component References
         [Header("Components")]
         [SerializeReference] private TMP_Text textComponent;
@@ -37,12 +39,39 @@ namespace Snowmentum.Score
         }
 
         /// <summary>
+        /// Converts an iteger score into a string with zeros appended to the beginning to fill a certain 
+        /// number of digits.
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <param name="score"></param>
+        /// <param name="postfixDigits">The number of arbituary 0s to add to the end of the score to give it an 
+        /// arcade feel.</param>
+        /// <returns></returns>
+        private static string ScoreToDigits(int digits, int score, int postfixDigits)
+        {
+            string scoreString = score.ToString();
+            Debug.Log(scoreString);
+            for (int i = scoreString.Length; i < digits; i ++)
+            {
+                scoreString = "0" + scoreString;
+            }
+            // Add Postfix digits.
+            for (int i = 0; i < postfixDigits; i++)
+            {
+                scoreString = scoreString + "0";
+            }
+
+            Debug.Log(scoreString);
+            return scoreString;
+        }
+
+        /// <summary>
         /// Updates the score text whenever the player's score changes.
         /// </summary>
         /// <param name="score"></param>
         private void UpdateScoreText(int score)
         {
-            textComponent.text = score.ToString();
+            textComponent.text = ScoreToDigits(displayedDigits, score, postfixDigits);
         }
     }
 }
