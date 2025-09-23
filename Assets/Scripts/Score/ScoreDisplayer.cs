8c234903cf13a7b04d6e6893d6bd594650dd6b40
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-22 20:13:51 -0500

    Score Display

--- Assets/Scripts/Score/ScoreDisplayer.cs
+++ Assets/Scripts/Score/ScoreDisplayer.cs
@@ -13,6 +13,37 @@ namespace Snowmentum.Score
 {
     public class ScoreDisplayer : MonoBehaviour
     {
-    
+        #region Component References
+        [Header("Components")]
+        [SerializeReference] private TMP_Text textComponent;
+
+        /// <summary>
+        /// Get components on reset.
+        /// </summary>
+        [ContextMenu("Get Component References")]
+        private void Reset()
+        {
+            textComponent = GetComponent<TMP_Text>();
+        }
+        #endregion
+
+
+        private void Awake()
+        {
+            ScoreStatic.OnScoreUpdated += UpdateScoreText;
+        }
+        private void OnDestroy()
+        {
+            ScoreStatic.OnScoreUpdated -= UpdateScoreText;
+        }
+
+        /// <summary>
+        /// Updates the score text whenever the player's score changes.
+        /// </summary>
+        /// <param name="score"></param>
+        private void UpdateScoreText(int score)
+        {
+            textComponent.text = score.ToString();
+        }
     }
 }
