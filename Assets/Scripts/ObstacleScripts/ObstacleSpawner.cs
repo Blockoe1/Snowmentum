c611f0e4c0f240babfb5d34d81fbe6d5ac6a9343
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-09-24 12:39:47 -0500

    Obstacle Spawning scales with speed.

--- Assets/Scripts/ObstacleScripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleScripts/ObstacleSpawner.cs
@@ -20,6 +20,7 @@ namespace Snowmentum
         
         [SerializeField] private int obstacleSpawnAmount = 1;  //amount of obstacles that will be spawned
         [SerializeField] private float spawnCooldown;  //cooldown on spawning obstacles so it isn't going constantly
+        [SerializeField] private bool scaleWithSpeed;
 
         //used to set the y axis area the obstacles can spawn in
         [SerializeField] private float minYSpawn;
@@ -78,12 +79,14 @@ namespace Snowmentum
         IEnumerator SpawnObstacles()
         {
             isSpawning = true;
+            float spawnDelay;
+            GameObject obstacleSpawn;
             while(isSpawning)
             {
                 for (int i = 0; i < obstacleSpawnAmount; i++)
                 {
                     //Pick an obstacle prefab
-                    GameObject obstacleSpawn = GetObstaclePrefab(obstacles);
+                    obstacleSpawn = GetObstaclePrefab(obstacles);
 
                     // If no obstacle is valid to be spawned right now, then we should skip spawning.
                     if (obstacleSpawn == null)
@@ -99,7 +102,8 @@ namespace Snowmentum
                     
                     //StartCoroutine(SpawnObstacles());
                 }
-                yield return new WaitForSeconds(spawnCooldown);
+                spawnDelay = scaleWithSpeed ? spawnCooldown / SnowballSpeed.TargetValue : spawnCooldown;
+                yield return new WaitForSeconds(spawnDelay);
             }
         }
 
