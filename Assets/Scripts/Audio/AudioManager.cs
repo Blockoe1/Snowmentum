998aa2dbb567821077bbbf544a604d414433de3d
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-10-03 16:19:52 -0500

    I lied snowball flashing

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : AudioManager.cs
+// Author :
+// Creation Date : 
+// Last Modified : 
+//
+// Brief Description : 
+*****************************************************************************/
+using UnityEngine;
+
+namespace Snowmentum
+{
+    public class AudioManager : MonoBehaviour
+    {
+    
+    }
+}
