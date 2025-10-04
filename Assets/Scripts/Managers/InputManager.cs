fb2405b5078b6b18b21199fc40d8dd6eb0d4143f
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-10-04 18:26:20 -0500

    InputManager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -6,6 +6,7 @@
 //
 // Brief Description : Centralized place to read framerate independent mouse delta input
 *****************************************************************************/
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,6 +14,40 @@ namespace Snowmentum
 {
     public class InputManager : MonoBehaviour
     {
-        private InputAction mouseDelta;
+        #region CONSTS
+        private const string DELTA_ACTION_NAME = "MouseMovement";
+        #endregion
+
+        private static Vector2 mouseDelta;
+        
+        private InputAction mouseDeltaAction;
+
+        #region Events
+        public static event Action<Vector2> OnDeltaUpdate;
+        #endregion
+
+        #region Properties
+        public static Vector2 MouseDelta => mouseDelta;
+        #endregion
+
+        private void Awake()
+        {
+            mouseDeltaAction = InputSystem.actions.FindAction(DELTA_ACTION_NAME);
+        }
+
+        /// <summary>
+        /// Continually update our mouse delta value in update so that we can get a frame independent value.
+        /// </summary>
+        /// <remarks>
+        /// Update boooo.
+        /// </remarks>
+        private void Update()
+        {
+            // Use Time.deltaTime here to average the delta into pixels / second.
+            // Without this we get framerate dependence
+            mouseDelta = mouseDeltaAction.ReadValue<Vector2>() / Time.deltaTime;
+            OnDeltaUpdate?.Invoke(mouseDelta);
+        }
+
     }
 }
