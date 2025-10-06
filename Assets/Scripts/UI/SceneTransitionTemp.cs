8becf3612ce7d7fb2dcf500198b282184101abf0
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-10-06 12:20:10 -0500

    Temp Scene transition script

---
+++ Assets/Scripts/UI/SceneTransitionTemp.cs
@@ -0,0 +1,21 @@
+/*****************************************************************************
+// File Name : SceneTransitionTemp.cs
+// Author :
+// Creation Date : 
+// Last Modified : 
+//
+// Brief Description : 
+*****************************************************************************/
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Snowmentum
+{
+    public class SceneTransitionTemp : MonoBehaviour
+    {
+        public void TransitionToScene(string targetScene)
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
