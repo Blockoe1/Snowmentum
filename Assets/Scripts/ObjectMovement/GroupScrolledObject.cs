7431e8be5723c3e15ef6186955d29d30e8261b48
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-10-16 12:48:21 -0500

    Audio Relay Update

---
+++ Assets/Scripts/ObjectMovement/GroupScrolledObject.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : GroupScrolledObject.cs
+// Author :
+// Creation Date : 
+// Last Modified : 
+//
+// Brief Description : 
+*****************************************************************************/
+using UnityEngine;
+
+namespace Snowmentum
+{
+    public class GroupScrolledObject : MonoBehaviour
+    {
+    
+    }
+}
