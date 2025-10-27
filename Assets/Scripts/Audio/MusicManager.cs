cc6f9733dfc3675a6c75277489fb7badfdb2d2cf
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-10-26 22:49:30 -0500

    Script Audio Folder

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,17 @@
+/*****************************************************************************
+// File Name : MusicManager.cs
+// Author :
+// Creation Date : 
+// Last Modified : 
+//
+// Brief Description : 
+*****************************************************************************/
+using UnityEngine;
+
+namespace Snowmentum
+{
+    public class MusicManager : MonoBehaviour
+    {
+    
+    }
+}
