cda8fa2186783935a2c09ad4c7d660df4786e555
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-10-29 11:29:13 -0500

    Update ObstacleController.cs

--- Assets/Scripts/Obstacles/ObstacleController.cs
+++ Assets/Scripts/Obstacles/ObstacleController.cs
@@ -279,6 +279,7 @@ namespace Snowmentum
         /// <summary>
         /// Reads the data from this object's obstacleData and updates the components on this GameObject.
         /// </summary>
+        [ContextMenu("Read Obstacle Data")]
         private void Editor_ReadObstacleData()
         {
             if (obstacleData == null) { return; }
