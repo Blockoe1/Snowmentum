1cfeaf60a9fea441c53f3a2b1a0e8c9aab6bfd63
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-11-14 15:29:20 -0600

    Survey Tools

--- Assets/Scripts/Snowball/SnowballProxy.cs
+++ Assets/Scripts/Snowball/SnowballProxy.cs
@@ -7,6 +7,7 @@
 // Brief Description : Script that mimicks snowball growth so that we can get screenshots for an obstacle size survey.
 *****************************************************************************/
 using Snowmentum.Size;
+using System.Linq;
 using UnityEngine;
 
 namespace Snowmentum
@@ -15,10 +16,23 @@ namespace Snowmentum
     {
         [SerializeField] private int bracket;
         [SerializeField] private float size;
-        [Tooltip("Internal Data")]
+        [Header("Internal Data")]
         [SerializeField] private Vector3 referenceScale = new Vector3(0.2f, 0.2f, 0.2f);
         [SerializeField] private UpdateSprite[] updateSprites;
 
+        #region Component References
+        [Header("Components")]
+        [SerializeReference] protected SpriteRenderer rend;
+
+        /// <summary>
+        /// Get components on reset.
+        /// </summary>
+        [ContextMenu("Get Component References")]
+        private void Reset()
+        {
+            rend = GetComponent<SpriteRenderer>();
+        }
+        #endregion
 
         #region Nested
         [System.Serializable]
@@ -31,9 +45,19 @@ namespace Snowmentum
 
         private void OnValidate()
         {
-            // Sets the snowball scale based on it's size and our environment size.
-            transform.localScale = referenceScale * (size / SizeBracket.GetMinSize(bracket));
+            // Sets the snowball scale based on it's size and our environment size
+            float sVal = size / SizeBracket.GetMinSize(bracket);
+            transform.localScale = referenceScale * sVal;
             // Update the sprite based on the UpdateSprites array.
+            updateSprites = updateSprites.OrderBy(item => item.transitionSize).Reverse().ToArray();
+            foreach (var item in updateSprites)
+            {
+                if (sVal > item.transitionSize)
+                {
+                    rend.sprite = item.sprite;
+                    break;
+                }
+            }
         }
     }
 }
