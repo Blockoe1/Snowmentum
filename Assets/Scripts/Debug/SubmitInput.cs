1144c0d032c9e9246cbee8053ce995176178828e
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-12-04 16:26:55 -0600

    Recorder Import

--- Assets/Scripts/Debug/SubmitInput.cs
+++ Assets/Scripts/Debug/SubmitInput.cs
@@ -32,6 +32,7 @@ namespace Snowmentum
         private void SubmitAction_performed(InputAction.CallbackContext obj)
         {
             onSubmit?.Invoke();
+            //TransitionManager.LoadScene("TransitionFootageScene", TransitionType.Snowy);
             //isPaused = !isPaused;
             //if (isPaused)
             //{
